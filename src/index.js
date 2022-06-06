ca55605acba20ec41b17daa67872f9d9ea8db6ed
dwarfsuomalainen <73884598+dwarfsuomalainen@users.noreply.github.com>
2022-06-06 13:51:59 +0300

    textfield

--- src/index.js
+++ src/index.js
@@ -1,41 +1,5 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<h1>Hello World!</h1>
-<div>
-<table> 
-  <tr>
-    <th>Name</th>
-    <th>Price</th>
-  </tr>
-  <tr>
-    <td>Meal 1</td>
-    <td>4,23 €</td>
-  </tr>
-
-<tr>
-  <td>Meal 2</td>
-  <td>5,23 €</td>
-</tr>
-
-<tr>
-  <td>Meal 3</td>
-  <td>5,55 €</td>
-</tr>
-
-<tr>
-  <td>Meal 4</td>
-  <td>6,77 €</td>
-</tr>
-
-<tr>
-  <td>Meal 5</td>
-  <td>6,88 €</td>
-</tr>
-
-  </table>
-  
-    
-      
-</div>
 `;
+let a = document.getElementById("textarea");
