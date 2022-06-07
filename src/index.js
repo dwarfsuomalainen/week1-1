03bb6d73ff316a2039db27018b237234bc6faab4
dwarfsuomalainen <73884598+dwarfsuomalainen@users.noreply.github.com>
2022-06-07 12:08:51 +0300

    button and text field

--- src/index.js
+++ src/index.js
@@ -2,4 +2,31 @@ import "./styles.css";
 
 document.getElementById("app").innerHTML = `
 `;
-let a = document.getElementById("textarea");
+//let a = document.getElementById("textarea");
+//console.log(a);
+//let b = a.innerText;
+
+//let button = document.getElementById("button");
+//console.log(button);
+//button.addEventListener("click", postToList);
+let list1 = document.createElement("ul");
+list1.id = "list";
+document.body.appendChild(list1);
+let inputField = document.createElement("input");
+inputField.id = "textarea";
+inputField.placeholder = "enter comment here";
+document.body.appendChild(inputField);
+let button = document.createElement("button");
+button.id = "add-comment";
+button.innerHTML = "add your comment";
+document.body.appendChild(button);
+let text = document.getElementById("textarea");
+button.addEventListener("click", addComment);
+function addComment() {}
+
+//.appendChild (textField);
+
+function postToList() {
+  let list = document.getElementById("list");
+  list.innerHTML = b;
+}
