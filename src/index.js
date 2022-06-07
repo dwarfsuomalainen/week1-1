dfb31519c5eacf0e485120bf93c49616cb23f322
dwarfsuomalainen <73884598+dwarfsuomalainen@users.noreply.github.com>
2022-06-07 12:34:59 +0300

    adding a comment to the list

--- src/index.js
+++ src/index.js
@@ -12,17 +12,21 @@ document.getElementById("app").innerHTML = `
 let list1 = document.createElement("ul");
 list1.id = "list";
 document.body.appendChild(list1);
-let inputField = document.createElement("input");
-inputField.id = "textarea";
-inputField.placeholder = "enter comment here";
-document.body.appendChild(inputField);
+//let inputField = document.createElement("input");
+//inputField.id = "textarea";
+//inputField.placeholder = "enter comment here";
+//document.body.appendChild(inputField);
 let button = document.createElement("button");
 button.id = "add-comment";
 button.innerHTML = "add your comment";
 document.body.appendChild(button);
 let text = document.getElementById("textarea");
 button.addEventListener("click", addComment);
-function addComment() {}
+function addComment() {
+  const li = document.createElement("li");
+  list1.appendChild(li);
+  //list1.innerHTML = .innerHTML;
+}
 
 //.appendChild (textField);
 
